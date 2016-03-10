001f5314e158b3aa06f72b3196219ef84a28d90a
jungmtg <cardofang@gmail.com>
2016-03-10 18:35:02 +0800

    Fix

--- HomeworkWeek1/Controllers/CustomerController.cs
+++ HomeworkWeek1/Controllers/CustomerController.cs
@@ -10,131 +10,132 @@ using HomeworkWeek1.Models;
 
 namespace HomeworkWeek1.Controllers
 {
-    public class CustomerController : Controller
-    {
-        private 客戶資料Entities db = new 客戶資料Entities();
+	public class CustomerController : Controller
+	{
+		private 客戶資料Entities db = new 客戶資料Entities();
 
 		// GET: Customer
 		//public ActionResult Index()
 		//{
 		//    return View(db.客戶資料.Where(c=>c.是否已刪除== false).ToList());
 		//}
-		public ActionResult Index(string customerName)
+		public ActionResult Index(string customerName, string uniNumber,string tel,string fax)
 		{
 			var result = db.客戶資料.Where(c => c.是否已刪除 == false);
 			if (!string.IsNullOrEmpty(customerName))
 				result = result.Where(c => c.客戶名稱 == customerName);
-			//return View(db.客戶資料.Where(c => c.是否已刪除 == false && c.客戶名稱 == customerName).ToList());
-			else
-				result = result;
-			//return View(db.客戶資料.Where(c => c.是否已刪除 == false).ToList());
-
-				return View(result.ToList());
+			if (!string.IsNullOrEmpty(uniNumber))
+				result = result.Where(c => c.統一編號 == uniNumber);
+			if (!string.IsNullOrEmpty(tel))
+				result = result.Where(c => c.電話 == tel);
+			if (!string.IsNullOrEmpty(fax))
+				result = result.Where(c => c.傳真 == fax);
+			return View(result.ToList());
 		}
 
-        // GET: Customer/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
+		// GET: Customer/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			客戶資料 客戶資料 = db.客戶資料.Find(id);
+			if (客戶資料 == null)
+			{
+				return HttpNotFound();
+			}
+			return View(客戶資料);
+		}
 
-        // GET: Customer/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: Customer/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Customer/Create
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
-        {
-            if (ModelState.IsValid)
-            {
-                db.客戶資料.Add(客戶資料);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		// POST: Customer/Create
+		// 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+		// 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+		{
+			if (ModelState.IsValid)
+			{
+				db.客戶資料.Add(客戶資料);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 
-            return View(客戶資料);
-        }
+			return View(客戶資料);
+		}
 
-        // GET: Customer/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
+		// GET: Customer/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			客戶資料 客戶資料 = db.客戶資料.Find(id);
+			if (客戶資料 == null)
+			{
+				return HttpNotFound();
+			}
+			return View(客戶資料);
+		}
 
-        // POST: Customer/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(客戶資料).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(客戶資料);
-        }
+		// POST: Customer/Edit/5
+		// 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+		// 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(客戶資料).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(客戶資料);
+		}
 
-        // GET: Customer/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-            if (客戶資料 == null)
-            {
-                return HttpNotFound();
-            }
-            return View(客戶資料);
-        }
+		// GET: Customer/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			客戶資料 客戶資料 = db.客戶資料.Find(id);
+			if (客戶資料 == null)
+			{
+				return HttpNotFound();
+			}
+			return View(客戶資料);
+		}
 
-        // POST: Customer/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            客戶資料 客戶資料 = db.客戶資料.Find(id);
-	        客戶資料.是否已刪除 = true;
+		// POST: Customer/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			客戶資料 客戶資料 = db.客戶資料.Find(id);
+			客戶資料.是否已刪除 = true;
 			db.Entry(客戶資料).State = EntityState.Modified;
 			db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
